77869263cce0ecc64a338d3305a5eefa55854414
Moeeze Hassan <fammas.maz@gmail.com>
2023-03-16 11:36:35 +0100

    [TP2] add reference solution (tag:w)
    Signed-off-by: Moeeze Hassan <fammas.maz@gmail.com>

---
+++ TP1_eleve/POD_eleve.m
@@ -0,0 +1,83 @@
+%% *POD applied to an image*
+%% 
+% _V. Matray_
+
+clear all
+close all
+clc
+%% POD by SVD
+% load original image
+
+UCimage = imread('callas.jpg');
+UCimage = im2double(UCimage(:,:,1));
+figure
+subplot(1,2,1)
+colormap(gray); imagesc(UCimage(:,:));axis equal;axis tight; axis off;
+title ('Maria Callas')
+%% 
+% get dimensions of the image
+
+n_T = size(UCimage, 1);
+n_M = size(UCimage, 2);
+U = UCimage;
+%% 
+% SVD on the image
+
+[phi,S,V] = svd(U);
+%% 
+% Order for reconstruction ?
+
+order = 25;
+%% 
+% Initialize the reconstruction 
+
+Ui = zeros(n_M,n_T)';
+%% 
+% Reconstruction :
+
+for o = 1:order
+    for i = 1:n_T
+       Ui(:,i) =  Ui(:,i)+ phi(:,o)*(U(:,i)'*phi(:,o)) ;
+    end
+Ui = Ui +S(o,o)*phi(:,o)*V(:,o)';
+end
+%% 
+% Optional challenge: build Ui using a single Matlab line 
+
+%TODO
+%% 
+% plot
+
+subplot(1,2,2)
+colormap(gray); imagesc(Ui(:,:));axis equal;axis tight; axis off;
+title("Approximation avec "+order+" modes")
+%% Calculation of the reconstruction error
+% Plot the reconstruction error as a function of the number of modes used. The 
+% reconstruction error considered here will be calculated with the norm of your 
+% choice (you can use the "norm" function of Matlab it's the fastest/ easiest 
+% way ;) ). 
+
+%TODO
+M = (S*phi*V');
+Mord = zeros(size(M));
+for o = 1:order
+    Mord = Mord + S(o,o)*phi(:,o)*V(:,o)'
+end
+
+e = norm(Mord - M)/norm(M);
+%% 
+% The evolution of the reconstruction error
+
+order = 25
+e = []
+sior = n_M*n_T;
+sizes = [];
+for o=1:50    
+    Ui = zeros(size(M));
+    Ui(:,:) = phi(:, 1:o)*S(1:o, 1:o)*V(:, 1:o)';
+    e = [e , norm(U-Ui)/norm(U)];
+    sizes = [sizes sior-(o*(n_T+n_M+1))];
+end
+hold off
+figure
+plot(e)
--- TP2_eleve/TP2.m
+++ TP2_eleve/TP2.m
@@ -1,31 +1,89 @@
+clear; close all; clc;
+
+%% Global parameters
+E = 1;
+S = 1;
+if_force = 1;
+
 %% Grid definition
 
-L = 1.0 % length of the domain
-T = 1.0 % final time
+L = 1.0; % length of the domain
+T = 1.0; % final time
 nx = 1000; % number of grid points
 nt = 1000; % number of time steps
 
 lx0 = 0.0; % left boundary
 lt0 = 0.0; % initial time
 
-dellx = L/(nx-1); % grid spacing
-dellt = T/(nt-1); % time step
+dx = L/(nx-1); % grid spacing
+dt = T/(nt-1); % time step
+
+Ix = sparse(nx, nx);
+It = sparse(nt, nt);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Time steps
-lx = lx0:dellx:L; % grid points
-lt = lt0:dellt:T; % time points
+lx = lx0:dx:L; % grid points
+lt = lt0:dt:T; % time points
 
-
-f = zeros(nx, nt); % solution matrix 
-
-%% Continuous initial condition
-ud0 = @(t) sin(2*pi*t)/T; % initial condition
-udL = @(t) -sin(4*pi*t)/T; % boundary condition
+%% Matrix initialization
+f = sparse(nx, nt); % force matrix 
+K = sparse(nx, nx); % stiffness matrix
+u = sparse(nx, nt); % solution matrix
 
 %% discrete initial condition
 ud0d = sin(2*pi*lt)/T; % initial condition
 udLd = -sin(4*pi*lt)/T; % boundary condition
+
+%% discrete force
+if if_force==1
+    f = (1e3 * (sin(3*pi*lt)/T)' * (sin(5*pi*lx)/L))';
+end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%% Constructing the Identities for Assembly
+
+mx = dx/6*[2 1; 1 2];
+mt = dt/6*[2 1; 1 2];
+
+for i = 1:nx-1
+    Ix(i:i+1, i:i+1) = Ix(i:i+1, i:i+1) + mx;
+end
+for i = 1:nt-1
+    It(i:i+1, i:i+1) = It(i:i+1, i:i+1) + mt;
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Assembly of F and K
+
+F = Ix * f;
+ke = 1/dx*[1 -1; -1 1];
+
+for i = 1:nx-1
+    K(i:i+1, i:i+1) = K(i:i+1, i:i+1) + ke;
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Refactoring the solution matrix
+u(1, :) = ud0d;
+u(end, :) = udLd;
+dof_b = [1, nx];
+dof_u = setdiff(1:nx, dof_b);
+Kuu = K(dof_u, dof_u);
+Kub = K(dof_u, dof_b);
+Kbu = K(dof_b, dof_u);
+Kbb = K(dof_b, dof_b);
+
+Fu = F(dof_u, :);
+u(dof_u, :) = Kuu\(Fu - Kub*u(dof_b, :));
+
+figure
+[lx_mesh, lt_mesh] = meshgrid(lx, lt);
+mesh(lx_mesh, lt_mesh, u');
+xlabel('x')
+ylabel('t')
+zlabel('u')
+title(['Solution of the 1D wave equation with force = ', num2str(if_force)]);
+saveas(gcf, strcat('../Final Report/assets/TP2_ref_solution_', num2str(if_force), '.png'));
+saveas(gcf, strcat('assets/TP2_ref_solution_', num2str(if_force), '.png'));
 
